fa4bff8fbc0a937fa9f1bc53b644506b7bf99c7c
Shrirang <Shrirang@DESKTOP-M087BV7>
2022-07-31 18:01:03 +0530

    fashion

---
+++ src/abcd/str.java
@@ -0,0 +1,14 @@
+package abcd;
+
+public class str
+{
+	public static void main(String[ ]args)
+	{
+		System.out.println("welcome");
+		System.out.println("welcome");
+		System.out.println("welcome");
+		System.out.println("welcome");
+	}	
+	
+
+}
