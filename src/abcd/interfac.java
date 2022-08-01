1c17d370bc292c92aaca8c3265fa6dae7fcbc0a0
Shrirang <Shrirang@DESKTOP-M087BV7>
2022-08-01 14:27:35 +0530

    interfac

---
+++ src/abcd/interfac.java
@@ -0,0 +1,17 @@
+package abcd;
+
+public class interfac
+{
+	public static void main(String[ ]args)
+	{
+		System.out.println("welcome to garden and diner");
+		
+		System.out.println("welcome to kop and temple");
+		
+		System.out.println("welcome to pune city");
+		
+		System.out.println("welcome to screen in the world");
+	}
+
+
+}
