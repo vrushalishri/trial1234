dd8412da1da9b836ce228c812a53fe50443aa696
Shrirang <Shrirang@DESKTOP-M087BV7>
2022-08-01 13:45:02 +0530

    kop

---
+++ src/abcd/strin1.java
@@ -0,0 +1,16 @@
+package abcd;
+
+public class strin1
+{
+	public static void main(String[ ]args)
+	{
+		System.out.println("welcome to garden");
+		
+		System.out.println("welcome to kop");
+		
+		System.out.println("welcome to mandir");
+		
+		System.out.println("welcome to screen");
+	}
+
+}
